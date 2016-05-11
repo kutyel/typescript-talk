abc108e2608f9f2bdec7c10ec240f75c8a32e18b
kutyel <flaviocorpa@gmail.com>
2016-05-11 22:50:53 +0200

    TypeScript + SystemJS

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { Person } from './person';
+
+let people: Person[] = [];
+
+people.push(new Person('Flavio', 25));
+people.push(new Person('David', 22));
+
+console.log(people);
---
+++ src/person.ts
@@ -0,0 +1,16 @@
+export class Person {
+
+    /**
+     * attributes
+     */
+    private _name: string;
+    private _age: number;
+
+    /**
+     * constructor
+     */
+    constructor(name: string, age: number) {
+        this._name = name;
+        this._age = age;
+    }
+}
