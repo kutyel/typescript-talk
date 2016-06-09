806f8aef17483e2f1254539d11478dca9c30c82f
kutyel <flaviocorpa@gmail.com>
2016-06-09 23:06:25 +0200

    add typings for leaflet

--- src/geolocation.ts
+++ src/geolocation.ts
@@ -1,23 +1,23 @@
 export class Geolocator {
 
-    private navigator: Navigator;
+    private _navigator: Navigator;
 
     constructor() {
         if (this._isGeolocationAvailable()) {
-            this.navigator = navigator;
+            this._navigator = navigator;
         } else {
-            console.log('Your browser does not support geolocation.');
+            console.log("Your browser does not support geolocation.");
         }
     }
 
     public getPosition(): Promise<Coordinates> {
         return new Promise((resolve, reject) =>
-            this.navigator.geolocation
+            this._navigator.geolocation
                 .getCurrentPosition(({ coords, timestamp }) => resolve(coords),
-                () => reject('We could not get your location.')));
+                () => reject("We could not get your location.")));
     }
 
     private _isGeolocationAvailable(): boolean {
-        return 'geolocation' in navigator;
+        return "geolocation" in navigator;
     }
 }
--- src/map.ts
+++ src/map.ts
@@ -1,25 +1,23 @@
-declare var L: any; // TODO: I want to have leaflet typings!!!
-
-import { Geolocator } from './geolocation';
+import { Geolocator } from "./geolocation";
 
 let geolocator = new Geolocator();
 
 export async function init() {
-    
-    try {        
+
+    try {
         // get current location
 
         let { latitude, longitude } = await geolocator.getPosition();
 
         // show position in map
 
-        let map = L.map('map').setView([latitude, longitude], 13);
+        let map = L.map("map").setView([latitude, longitude], 13);
 
-        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        L.tileLayer("http://{s}.tile.osm.org/{z}/{x}/{y}.png", {
+            attribution: `&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors`
         }).addTo(map);
 
-        L.marker([latitude, longitude]).addTo(map).bindPopup('You are here.').openPopup();
+        L.marker([latitude, longitude]).addTo(map).bindPopup("You are here.").openPopup();
 
     } catch (err) {
         console.error(err);
